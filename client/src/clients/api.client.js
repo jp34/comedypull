3a4c8243b6b7c4a9acd20aa3fe753ca048f63b2e
jp34 <joekadlic@outlook.com>
2024-01-31 12:49:35 -0500

    Updated nearby shows url

--- client/src/clients/api.client.js
+++ client/src/clients/api.client.js
@@ -3,7 +3,7 @@ import client from "config/axios.config";
 export const fetchNearbyShows = async (latitude, longitude, n) => {
     try {
         const params = new URLSearchParams({ latitude, longitude, size: n });
-        const response = await client.get(`/s?${params.toString()}`);
+        const response = await client.get(`/nearby/shows?${params.toString()}`);
         return response.data;
     } catch (error) {
         console.log(`Failed to request nearby shows - ${error.message}`);
